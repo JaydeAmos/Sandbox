8b2b633da6a80a751d6fee7bb6ff2ddc98a8f75a
jayde <jayde.amos@my.jcu.edu.au>
2019-08-20 09:37:09 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,2 +1,13 @@
 """ Jayde Amos """
 
+MIN_LENGTH = 5
+
+
+def main():
+    password = input("Enter password of at least 5 characters: ")
+    while len(password) < MIN_LENGTH:
+        password = input("Enter password of at least 5 characters: ")
+    print("You password is", '*' * len(password))
+
+
+main()
